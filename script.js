738e3d2b04f21c0c2cb4f5d6cdb3a44b75e98ca5
Denis Akturk <denisakturk@yandex.ru>
2019-11-03 18:22:50 +0300

    Slider menu animation

--- script.js
+++ script.js
@@ -5,20 +5,29 @@ const navSlide = () => {
     const cart = document.querySelector('.cart');
     const cartInner = document.querySelector('.cart__inner');
     // Navigation toggle
-    burg.addEventListener('click', () => {
-        nav.classList.toggle('nav-list__active');
-        // Animation of links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = ''
-            } else {
-                link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s';
-            }
-        });
+    // burg.addEventListener('click', () => {
+    //     nav.classList.toggle('nav-list__active');
+    //     // Animation of links
+    //   
+    // });
+    document.addEventListener("click", function(e) {
+        let m = document.getElementById('nav-list');
+        if (e.target.id != 'ham-menu' && e.target.id != 'nav-list') {
+          m.style.display = 'none';
+        } else if (e.target.id == 'ham-menu') {
+          m.style.display = (m.style.display != 'flex') ? 'flex' : 'none';
+        }
+        // navLinks.forEach((link, index) => {
+        //     if (link.style.animation) {
+        //         link.style.animation = ''
+        //     } else {
+        //         link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s';
+        //     }
+        // });
     });
     cart.addEventListener('click', () => {
-        cartInner.style.display = 'flex';
+        cartInner.classList.toggle('cart-inner__active');
     });
 }
-
 navSlide();
+
