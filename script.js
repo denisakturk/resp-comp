3441724fdd07069981c3b92dddcf7dcb166ee280
Denis Akturk <denisakturk@yandex.ru>
2019-11-03 05:33:25 +0300

    Shopping Cart

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const navSlide = () => {
     const burg = document.querySelector('.ham-menu');
     const nav = document.querySelector('.nav-list');
     const navLinks = document.querySelectorAll('.nav-list__link');
+    const cart = document.querySelector('.cart');
+    const cartInner = document.querySelector('.cart__inner');
     // Navigation toggle
     burg.addEventListener('click', () => {
         nav.classList.toggle('nav-list__active');
@@ -13,6 +15,9 @@ const navSlide = () => {
                 link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s';
             }
         });
+    });
+    cart.addEventListener('click', () => {
+        cartInner.style.display = 'flex';
     });
 }
 
